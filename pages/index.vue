fe17eb38575a10514bfb98e3db59558d092ef22a
A.Sch <a.sch@skbkontur.ru>
2020-11-26 19:06:01 +0500

    q
    q

--- pages/index.vue
+++ pages/index.vue
@@ -11,12 +11,35 @@
           </b-nav>
         </b-col>
         <b-col>
-          <b-container fluid class="pt-3">
+          <b-container fluid>
+            <b-row>
+              <b-col class="py-3">
+                <b-navbar toggleable="lg" type="light" variant="transparent" class="px-0">
+                  <b-navbar-brand href="#">
+                    <img src="~/static/icons/bird.svg" width="48px"/>
+                  </b-navbar-brand>
+                  <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+                  <b-collapse id="nav-collapse" is-nav>
+                    <b-navbar-nav>
+                      <b-nav-item href="#">Link</b-nav-item>
+                      <b-nav-item href="#" disabled>Disabled</b-nav-item>
+                    </b-navbar-nav>
+                    <b-navbar-nav class="ml-auto">
+                      <div>
+                          User
+                      </div>
+                    </b-navbar-nav>
+                  </b-collapse>
+                </b-navbar>
+              </b-col>
+            </b-row>
+          </b-container>
+          <b-container fluid>
             <b-row>
               <b-col lg="4" sm="12" class="mb-3 mb-lg-0" v-for="title in titles">
                 <b-card class="shadow-sm">
                   <b-card-title>{{ title }}</b-card-title>
-                  <hr>
+                  <hr class="my-1">
                   qweqwe
                 </b-card>
               </b-col>
