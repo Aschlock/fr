0d1af9273f444f616fdd5f41b549568887d20cb6
A.Sch <a.sch@skbkontur.ru>
2020-11-26 17:28:16 +0500

    bs
    bs

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,38 @@
 <template>
-  <div class="container">
-<h1>Jopa</h1>
+  <div>
+    <b-container fluid>
+      <b-row>
+        <b-col xl="1" lg="2" sm="12" class="vh-100 bg-dark d-none d-lg-block">
+          <b-nav vertical>
+            <b-nav-item active>Active</b-nav-item>
+            <b-nav-item>Link</b-nav-item>
+            <b-nav-item>Another Link</b-nav-item>
+            <b-nav-item disabled>Disabled</b-nav-item>
+          </b-nav>
+        </b-col>
+        <b-col>
+          <b-container fluid>
+            <b-row>
+              <b-col lg="4" sm="12">
+                <b-card header="123" bg-variant="">
+                  qweqwe
+                </b-card>
+              </b-col>
+              <b-col lg="4" sm="12">
+                <b-card header="123" bg-variant="">
+                  qweqwe
+                </b-card>
+              </b-col>
+              <b-col lg="4" sm="12">
+                <b-card header="123" bg-variant="">
+                  qweqwe
+                </b-card>
+              </b-col>
+            </b-row>
+          </b-container>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -10,12 +42,6 @@ export default {}
 
 <style>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
   color: #333;
 }
 
