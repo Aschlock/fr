f7fe5435c944851a99a6987196984aa0778daf36
A.Sch <a.sch@skbkontur.ru>
2020-12-02 18:31:45 +0500

    -
    -

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <b-container fluid>
       <b-row>
-        <div class="vh-100 bg-white d-none d-lg-block pt-3 border-right">
+        <div class="left-nav-menu vh-100 bg-white d-none d-lg-block pt-3 border-right">
           <b-nav vertical>
             <b-nav-item class="pl-1 pr-5" active>
               <b-icon icon="file-earmark-text" class="mr-3"></b-icon>
@@ -27,23 +27,30 @@
                 </h1>
               </b-col>
             </b-row>
+            <b-row class="mb-4">
+              <b-col cols="3" v-for="i in 4">
+                <b-card class="shadow-sm border-0">
+                  123
+                </b-card>
+              </b-col>
+            </b-row>
             <b-row>
-              <b-col xl="3" lg="4" sm="12" class="mb-3" v-for="card in cards" :key="card.id">
+              <b-col xl="3" lg="4" sm="12" class="mb-3">
                 <b-card body-class="px-4 py-3" class="shadow-sm border-0">
-                  <b-card-title class="d-flex">{{ card.title }}
-                    <b-button class="ml-auto font-weight-bold d-flex align-items-center" style="line-height: 1"
+                  <b-card-title class="d-flex">{{ payments.title }}
+                    <b-button class="ml-auto font-weight-bold d-flex align-items-center text-secondary-200" variant="secondary-100" style="line-height: 1"
                               @click="addTrans(), makeToast()">+
                     </b-button>
                   </b-card-title>
                   <hr class="my-1">
 
                   <b-list-group class="mx-n4 transactions">
-                    <template v-for="(item, index) in cards[0].items">
+                    <template v-for="(item, index) in payments.items">
                       <b-list-group-item class="flex-column align-items-start px-4 py-2" v-if="index < 5" :key="index">
                         <div class="d-flex w-100 justify-content-between">
                           <div>
                             <p class="mb-0">{{ item.payer }}</p>
-                            <small class="text-muted" :id="'tooltip-' + index">№{{ cards[0].items.length - index }}, {{ item.date.toLocaleDateString('ru') }}</small>
+                            <small class="text-muted" :id="'tooltip-' + index">№{{ payments.items.length - index }}, {{ item.date.toLocaleDateString('ru') }}</small>
                             <b-tooltip title="123" triggers="hover" :target="'tooltip-' + index">
                               {{ item.date.toLocaleString('ru') }}
                             </b-tooltip>
@@ -55,14 +62,14 @@
                         </div>
                       </b-list-group-item>
                     </template>
-                    <b-list-group-item href="#" v-if="cards[0].items.length > 5" class="text-primary">
+                    <b-list-group-item href="#" v-if="payments.items.length > 5" class="text-primary">
                       <div class="d-flex justify-content-between">
                         <div>
                           Показать все
                         </div>
                         <div>
-                          <b-badge variant="light" class="text-primary">
-                            {{ cards[0].items.length }}
+                          <b-badge variant="secondary-100" class="text-secondary-200">
+                            {{ payments.items.length }}
                           </b-badge>
                         </div>
                       </div>
@@ -89,7 +96,7 @@ export default {
   methods: {
     makeToast(append = false) {
       this.toastCount++
-      this.$bvToast.toast(`Новое списание №${this.cards[0].items.length} ${(new Date).toLocaleString('ru')}`, {
+      this.$bvToast.toast(`Новое списание №${this.payments.items.length} ${(new Date).toLocaleString('ru')}`, {
         title: 'My defence',
         autoHideDelay: 3000,
         appendToast: append
@@ -97,7 +104,7 @@ export default {
     },
 
     addTrans(sum = 1000) {
-      this.cards[0].items.unshift({
+      this.payments.items.unshift({
         isIncome: false,
         sum: (Math.random() * 1000).toFixed(0),
         payer: 'к ООО ААА',
@@ -107,7 +114,7 @@ export default {
   },
   data() {
     return {
-      cards: [
+      payments:
         {
           id: 1,
           title: 'Транзакции',
@@ -126,7 +133,7 @@ export default {
             },
           ]
         },
-      ]
+
     }
   }
 }
@@ -141,6 +148,12 @@ export default {
 .transactions .list-group-item:hover {
   background: whitesmoke;
   transition: .2s;
+}
+
+.left-nav-menu {
+  position: sticky;
+  top: 0;
+  overflow-y: auto;
 }
 
 </style>
