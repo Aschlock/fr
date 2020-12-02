f66278a61fe5f85b1370c4d1939b3219fa243400
A.Sch <a.sch@skbkontur.ru>
2020-12-03 03:39:10 +0500

    -

---
+++ components/Menu.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="left-nav-menu vh-100 bg-white d-none d-lg-block pt-3 border-right">
+    <b-nav vertical>
+      <b-nav-item class="pl-1 pr-5" active>
+        <b-icon icon="file-earmark-text" class="mr-3"></b-icon>
+        Документы
+      </b-nav-item>
+      <b-nav-item class="pl-1 pr-5">
+        <b-icon icon="bar-chart" class="mr-3"></b-icon>
+        Link
+      </b-nav-item>
+      <b-nav-item class="pl-1 pr-5" v-b-toggle="'collapse-2'">
+        <div class="dropdown-toggle">
+          <b-icon icon="bookmark" class="mr-3"></b-icon>
+          Another Link
+        </div>
+      </b-nav-item>
+      <b-collapse id="collapse-2">
+        <b-nav vertical>
+          <b-nav-item class="pl-3 pr-5" v-for="i in 15">
+            <b-icon icon="box-seam" class="mr-3"></b-icon>
+            Link
+          </b-nav-item>
+        </b-nav>
+      </b-collapse>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Menu"
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,32 +2,7 @@
   <div>
     <b-container fluid>
       <b-row>
-        <div class="left-nav-menu vh-100 bg-white d-none d-lg-block pt-3 border-right">
-          <b-nav vertical>
-            <b-nav-item class="pl-1 pr-5" active>
-              <b-icon icon="file-earmark-text" class="mr-3"></b-icon>
-              Документы
-            </b-nav-item>
-            <b-nav-item class="pl-1 pr-5">
-              <b-icon icon="bar-chart" class="mr-3"></b-icon>
-              Link
-            </b-nav-item>
-            <b-nav-item class="pl-1 pr-5" v-b-toggle="'collapse-2'">
-              <div class="dropdown-toggle">
-                <b-icon icon="bookmark" class="mr-3"></b-icon>
-                Another Link
-              </div>
-            </b-nav-item>
-            <b-collapse id="collapse-2">
-              <b-nav vertical>
-                <b-nav-item class="pl-3 pr-5" v-for="i in 15">
-                  <b-icon icon="box-seam" class="mr-3"></b-icon>
-                  Link
-                </b-nav-item>
-              </b-nav>
-            </b-collapse>
-          </b-nav>
-        </div>
+        <Menu />
         <b-col>
           <b-container fluid class="pt-3">
             <b-row class="mb-3">
@@ -99,8 +74,9 @@
 
 <script>
 import TaskTracker from "@/components/task-tracker";
+import Menu from "@/components/Menu";
 export default {
-  components: {TaskTracker},
+  components: {Menu, TaskTracker},
   head() {
     return {
       title: 'Хуячечная'
